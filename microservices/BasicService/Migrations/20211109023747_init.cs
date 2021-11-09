c3187229274c7be52cdf7f0e66a40091b2cf9ca0
topiv <topiv@DESKTOP-S2G72O3>
2021-11-09 15:25:55 +0800

    refactor(framework):更新框架版本

---
+++ microservices/BasicService/Migrations/20211109023747_init.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BasicService.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BasicDataDictionaries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    AppName = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Remark = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    Sort = table.Column<int>(type: "int", nullable: false),
+                    ExtraProperties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BasicDataDictionaries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BasicDataDictionaryDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Group = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: true),
+                    Remark = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    Sort = table.Column<int>(type: "int", nullable: false),
+                    ParentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BasicDataDictionaryDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BasicDataDictionaryDetails_BasicDataDictionaries_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "BasicDataDictionaries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasicDataDictionaries_Key",
+                table: "BasicDataDictionaries",
+                column: "Key");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasicDataDictionaryDetails_ParentId",
+                table: "BasicDataDictionaryDetails",
+                column: "ParentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BasicDataDictionaryDetails");
+
+            migrationBuilder.DropTable(
+                name: "BasicDataDictionaries");
+        }
+    }
+}
